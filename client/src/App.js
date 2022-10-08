272d251fa997f4501991d1b2360a347c112db427
vadim.cvirko <cvi-vadim@yandex.ru>
2022-10-08 10:07:04 +0300

    add loader ui

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,16 @@
 import React, { useEffect } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
+import Loader from './components/UI/Loader/Loader'
 import { loadGoodsList } from './store/goodsReducer'
 
 function App() {
   const dispatch = useDispatch()
   const data = useSelector((state) => state.goods.entities)
-  console.log(data);
   useEffect(() => { dispatch(loadGoodsList()) }, [dispatch])
   return (
-    <div className="App"> {data && data.map(item => (<p key={item._id}>{item.title}</p>))}</div>
+    // <div className="App"> {data && data.map(item => (<p key={item._id}>{item.title}</p>))}</div>
+
+    <Loader />
   );
 }
 
