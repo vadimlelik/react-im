5ab1b5d7b7970aa3a004ee565aebc7396e788695
vadim.cvirko <cvi-vadim@yandex.ru>
2022-10-12 18:26:08 +0300

    add login route and protected route

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,15 @@
 import React from 'react'
-import { Route, Routes } from 'react-router-dom';
+import { Navigate, Route, Routes } from 'react-router-dom';
 import Goodlist from './components/GoodsList/GoodList';
 import Layout from './components/Layout/Layout';
 import NotFound from './components/NotFound/NotFound'
 import Cart from './components/Cart/Cart';
 import ProtectedRoute from './components/ProtecdetRoute/ProtectedRoute';
 import GoodsLayout from './Layouts/GoodsLayout/GoodsLayout';
+import GoodsPage from './page/GoodsPage/GoodsPage';
+import AuthLayout from './Layouts/AuthLayout/AuthLayout';
+import LoginPage from './page/LoginPage/LoginPage';
+import SignUpPage from './page/SignUpPage/SignUpPage';
 
 function App() {
   return (
@@ -13,10 +17,19 @@ function App() {
       <Routes>
         <Route path='/cart' element={<Cart />} />
         <Route path='*' element={<NotFound />} />
-
         <Route path='/' element={<ProtectedRoute redirect={'/auth/login'} element={<GoodsLayout />} />}>
           <Route index element={<Goodlist />} />
+          <Route path=':goodId' element={<GoodsPage />} />
+          <Route path='edit' element={<h1>EditGoods</h1>} />
+          <Route path='new-goods' element={<h1>NewGoods</h1>} />
         </Route>
+        <Route path='auth' element={<AuthLayout />} >
+          {/* <Route index element={<Navigate to={'auth/signUp'} />} /> */}
+          <Route path='login' element={<LoginPage />} />
+          {/* <Route path='signUp' element={<SignUpPage />} /> */}
+          {/* <Route path='*' element={<Navigate to={'auth/signUp'} />} /> */}
+        </Route >
+
       </Routes>
     </Layout>
   );
--- client/src/components/ProtecdetRoute/ProtectedRoute.jsx
+++ client/src/components/ProtecdetRoute/ProtectedRoute.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Navigate } from "react-router-dom";
 
 const ProtectedRoute = ({ redirect, element }) => {
-  const isLoggedIn = false;
+  const isLoggedIn = true;
 
   if (!isLoggedIn) {
     return <Navigate to={redirect} />;
